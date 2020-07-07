7904421096cafe933decf527f1fc83861fae7b45
aecordova <ae_cordova@outlook.com>
2020-07-06 19:45:06 -0500

    Added Scenes to the template

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,15 @@
+import logoImg from '../assets/logo.png';
+
+export default class GameScene extends Phaser.Scene {
+  constructor() {
+    super('Game');
+  }
+
+  preload() {
+    this.load.image('logo', logoImg);
+  }
+
+  create() {
+    this.add.image(400, 300, 'logo');
+  }
+}
