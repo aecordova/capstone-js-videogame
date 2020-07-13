f7f4038c2674f5b850e0766654122e9e15a34497
aecordova <ae_cordova@outlook.com>
2020-07-13 18:49:59 -0500

    Fix Credits Scene alignment

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -1,4 +1,4 @@
-import 'phaser';
+import Phaser from 'phaser';
 import config from '../Config/config';
 
 export default class CreditsScene extends Phaser.Scene {
@@ -7,31 +7,24 @@ export default class CreditsScene extends Phaser.Scene {
   }
 
   create() {
+    const { width, height } = config;
+
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
-    this.zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+    this.madeByText = this.add.text(0, 0, 'Created By: Angel Cordova', { fontSize: '26px', fill: '#fff' });
 
-    Phaser.Display.Align.In.Center(
-      this.creditsText,
-      this.zone,
-    );
+    this.zone = this.add.zone(width / 2, height / 2, width, height);
 
-    Phaser.Display.Align.In.Center(
-      this.madeByText,
-      this.zone,
-    );
+    Phaser.Display.Align.In.Center(this.creditsText, this.zone);
+    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
 
     this.madeByText.setY(1000);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
-      y: -100,
+      y: -300,
       ease: 'Power1',
       duration: 3000,
       delay: 1000,
-      onComplete() {
-        this.destroy;
-      },
     });
 
     this.madeByTween = this.tweens.add({
@@ -41,7 +34,6 @@ export default class CreditsScene extends Phaser.Scene {
       duration: 8000,
       delay: 1000,
       onComplete: function () {
-        this.madeByTween.destroy;
         this.scene.start('Title');
       }.bind(this),
     });
